18acbde55b44fd4f830fb59c35b3f423ad883742
clonalejandro <contact@clonalejandro.me>
2017-09-11 17:07:36 +0200

    EntityNpc 0.1

--- src/main/java/me/clonalejandro/npcAPI/npc/EntityNpc.java
+++ src/main/java/me/clonalejandro/npcAPI/npc/EntityNpc.java
@@ -1,9 +1,20 @@
 package me.clonalejandro.npcAPI.npc;
 
+import com.mojang.authlib.GameProfile;
+
 import me.clonalejandro.npcAPI.utils.Manager;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by alejandrorioscalera
@@ -26,37 +37,221 @@ public class EntityNpc extends CraftNpc implements Entity {
 
     /** SMALL CONSTRUCTORS **/
 
-    private int entityID;
-    private Location location;
-    private String name;
-    private Player player;
+    private final int entityID;
+    private final Location location;
+    private final boolean hideTab;
+    private final String nameInTab;
+    private final boolean rotation;
+    private final GameProfile gameProfile;
+
+    private static Plugin plugin;
+    private static boolean started = false;
+
+    public static List<EntityNpc> npcs = new ArrayList<>();
+
+    private List<Player> rendered = new ArrayList<>();
+    private String tab;
+
+    /**
+     * This method is a constructor class
+     * @param name
+     * @param location
+     * @param rotation
+     * @param hideTab
+     * @param nameInTab
+     */
+    public EntityNpc(String name, Location location, boolean rotation, boolean hideTab, String nameInTab){
+        this.location = location;
+        this.rotation = rotation;
+        this.hideTab = hideTab;
+        this.nameInTab = nameInTab;
+
+        entityID = getID();
+        gameProfile = Manager.getGameProfile(name);
+
+        if(!npcs.contains(this))
+            npcs.add(this);
+    }
 
 
     /** REST **/
 
-    public void spawn() {
+    /**
+     * This method test if player rendered to an npc
+     * @param plugin
+     */
+    public static void Task(Plugin plugin){
+        if (!started){
+            started = true;
+            EntityNpc.plugin = plugin;
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    for (EntityNpc npc : npcs) {
+                        for (Player p : Bukkit.getOnlinePlayers()) {
+                            if (npc.location.getWorld().equals(p.getWorld())) {
+                                if (npc.location.distance(p.getLocation()) > 60 && npc.rendered.contains(p))
+                                    npc.destroy(p);
+                                else if (npc.location.distance(p.getLocation()) < 60 && !npc.rendered.contains(p))
+                                    npc.spawn(p, npc.rotation);
+                            } else npc.destroy(p);
+                        }
+                    }
+                }
+            }, 0L, 30L);
+        }
+    }
+
+
+    /**
+     * This method spawn an npc
+     * @param player
+     * @param rotation
+     */
+    @NPC
+    public void spawn(final Player player, boolean rotation) {
         try {
             final Class packet = Packet();
 
             configure(packet);
             Manager.sendPacket(player, packet);
 
-        } catch (Exception ex){
+            clearAI(player);
 
+            if (!hideTab) addTab(nameInTab);
+
+            if (rotation){
+                final float yaw = location.getYaw();
+
+                Class<?> headPacket = headRotationPacket();
+                setVal(headPacket, "a", entityID);
+                setVal(headPacket, "b", getYaw(yaw));
+
+                Manager.sendPacket(player, headPacket);
+            }
+
+            Bukkit.getScheduler().runTaskLater(EntityNpc.plugin, new Runnable(){
+                @Override
+                public void run() {
+                    removeTabList(player);
+                }
+            },26L);
+
+            rendered.add(player);
+
+            //TODO: Event
+        } catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 
 
-    public void destroy() {
+    /**
+     * This method destroy a npc
+     * @param player
+     */
+    @NPC
+    public void destroy(Player player) {
+        try {
+            Class<?> packet = packetDestroy();
+
+            removeTabList(player);
+            Manager.sendPacket(player, packet);
+
+            Class<?> scoreboard = Manager.getNMSClass("PacketPlayOutScoreboardTeam");
+            scoreboard.newInstance();
+
+            Field a = scoreboard.getDeclaredField("a");
+            Field h = scoreboard.getDeclaredField("h");
+
+            a.setAccessible(true);
+            a.set(scoreboard, gameProfile.getName());
+            a.setAccessible(false);
+
+            h.setAccessible(true);
+            h.set(scoreboard, 1);
+            h.setAccessible(false);
+
+            Manager.sendPacket(player, scoreboard);
+            rendered.remove(player);
+
+            //TODO: Event
+        }
+        catch (Exception ex){ ex.printStackTrace(); }
+    }
+
+
+    /**
+     * This method return gameProfile to npc
+     * @return
+     */
+    public GameProfile getGameProfile() {
+        return gameProfile;
+    }
+
+
+    /**
+     * This method return if npc is in RotationMode
+     * @return
+     */
+    public boolean isRotation() {
+        return rotation;
+    }
+
+
+    /**
+     * This method return if npc is Hidden in tab
+     * @return
+     */
+    public boolean isHideTab() {
+        return hideTab;
+    }
+
+
+    /**
+     * This method return to a Npcs List
+     * @return
+     */
+    public static List<EntityNpc> getNpcs() {
+        return npcs;
+    }
+
+
+    /**
+     * This method return to a render List players
+     * @return
+     */
+    public List<Player> getRendered() {
+        return rendered;
+    }
+
+
+    /**
+     * This method return to a npc Location
+     * @return
+     */
+    public Location getLocation(){
+        return location;
+    }
+
+
+    /**
+     * This method return a npc name in TabList
+     * @return
+     */
+    public String getNameInTab(){
+        return nameInTab;
     }
 
 
     /** OTHERS **/
 
+    /**
+     * This method configure a main packet
+     * @param packet
+     */
+    @NPC
     private void configure(Class packet){
-
-        World world = location.getWorld();
-
         final double x = location.getX(),
                      y = location.getY(),
                      z = location.getZ();
@@ -65,13 +260,201 @@ public class EntityNpc extends CraftNpc implements Entity {
                     pitch = location.getPitch();
 
         setVal(packet, "a", entityID);
-        setVal(packet, "b", entityID);//get profile ID
+        setVal(packet, "b", gameProfile.getId());//get profile ID
         setVal(packet, "c", getX(x));
         setVal(packet, "d", getY(y));
         setVal(packet, "e", getZ(z));
         setVal(packet, "f", getYaw(yaw));
         setVal(packet, "g", getPitch(pitch));
         setVal(packet, "i", getWatcher());
+    }
+
+
+    /**
+     * This method add to Tab an npc
+     * @param name
+     */
+    private void addTab(String name){
+        tab = Manager.translator(name);
+
+        final Class<?> packet = packetTab();
+        final Class<?> data = configurePacket(packet);
+        final Class<?> element = Manager.getNMSClass("PacketPlayOutPlayerInfo");
+        final Class[] elements = element.getClasses();
+
+        Object[] obj = null;
+        Object Enum = null;
+
+        for (Class<?> clazz : elements)
+            if (clazz.getName().equalsIgnoreCase("EnumPlayerInfoAction"))
+                obj = clazz.getEnumConstants();
+
+        assert obj != null;
+
+        for (Object object : obj)
+            if (object.toString().equalsIgnoreCase("ADD_PLAYER"))
+                Enum = object;
+
+        assert Enum != null;
+
+        @SuppressWarnings("unchecked")
+        List<Class<?>> players = (List<Class<?>>) getVal(packet, "b");
+        players.add(data);
+
+        setVal(packet, "a", Enum);
+        setVal(packet, "b", players);
+    }
+
+
+    /**
+     * This method return to a packetTab
+     * @return
+     */
+    private Class<?> packetTab() {
+        try {
+            Class clazz = Manager.getNMSClass("PacketPlayOutPlayerInfo");
+            clazz.newInstance();
+            return clazz;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * This method configure and return Data packet for a packetTab
+     * @param packet
+     * @return
+     */
+    private Class<?> configurePacket(Class<?> packet){
+        try {
+            Class<?>[] Predata = packet.getClasses();
+
+            Class<?> Data = null;
+            Class<?> Enum = Manager.getNMSClass("EnumGamemode");
+            Class<?> craftChatMessage = Manager.getBukkitNMSClass("CraftChatMessage");
+            Class<?> ChatBaseComponent;
+
+            Object[] vals = Enum.getEnumConstants();
+            Object val = null;
+
+            Method fromString = craftChatMessage.getDeclaredMethod("fromString", String.class);
+            Array array = (Array) fromString.invoke(craftChatMessage, tab);
+            ChatBaseComponent = (Class<?>) Array.get(array, 0);
+
+            for (Object object : vals)
+                if (object.toString().equalsIgnoreCase("NOT_SET"))
+                    val = object;
+
+            for (Class<?> clazz : Predata)
+                if (clazz.getName().equalsIgnoreCase("PlayerInfoData"))
+                    Data = clazz;
+
+            assert Data != null;
+            assert val != null;
+
+            Data.getConstructor(GameProfile.class, int.class, Object.class, Class.class).
+                    newInstance(gameProfile, 1, val, ChatBaseComponent);
+
+            return Data;
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * This method return an HeadRotation packet
+     * @return
+     */
+    private Class<?> headRotationPacket(){
+        try { return Manager.getNMSClass("PacketPlayOutEntityHeadRotation"); }
+        catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * This method cancel AI when player damage a npc
+     * @param player
+     */
+    private void clearAI(Player player){
+        try {
+            Class<?> packet = Manager.getNMSClass("PacketPlayOutScoreboardTeam");
+            packet.newInstance();
+
+            setVal(packet,"a", gameProfile.getName());
+            setVal(packet, "b", gameProfile.getName());
+            setVal(packet, "e", "never");
+            setVal(packet, "h", 0);
+            setVal(packet, "i", 1);
+
+            Field g = packet.getDeclaredField("g");
+            g.setAccessible(true);
+
+            ((Collection) g.get(packet)).add(gameProfile.getName());
+
+            Manager.sendPacket(player, packet);
+        }
+        catch (Exception ex){ ex.printStackTrace(); }
+    }
+
+
+    /**
+     * This method return a PacketDestroy
+     * @return
+     */
+    private Class<?> packetDestroy(){
+        try {
+            Class<?> packet = Manager.getNMSClass("PacketPlayOutEntityDestroy");
+            packet.getConstructor(int[].class).newInstance((Object) new int[] {entityID});
+            return packet;
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     * This method remove a npc From Tab List
+     * @param player
+     */
+    private void removeTabList(Player player){
+        try {
+            Class<?> packet = packetTab();
+            Class<?> Data = configurePacket(packet);
+
+            @SuppressWarnings("unchecked")
+            List<Class<?>> players = (List<Class<?>>) getVal(packet, "b");
+            players.add(Data);
+
+            Class[] Enum = Manager.getNMSClass("PacketPlayOutPlayerInfo").getClasses();
+            Class<?> rem = null;
+
+            for (Class<?> clazz : Enum)
+                if (clazz.getName().equalsIgnoreCase("EnumPlayerInfoAction"))
+                    rem = clazz;
+
+            assert rem != null;
+            Object[] enums = rem.getEnumConstants();
+            Object remove = null;
+
+            for (Object object : enums)
+                if (object.toString().equalsIgnoreCase("REMOVE_PLAYER"))
+                    remove = object;
+
+            assert remove != null;
+            setVal(packet, "a",remove);
+            setVal(packet, "b", players);
+
+            Manager.sendPacket(player, packet);
+        }
+        catch (Exception ex){ ex.printStackTrace(); }
     }
 
 
